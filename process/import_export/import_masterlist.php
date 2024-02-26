e37796c9f210ede6152970beca29c41a8ca2845b
jirish18 <irishbautista1801@gmail.com>
2024-02-26 10:11:28 +0800

    IMPORT MASTERLIST OK

--- process/import_export/import_masterlist.php
+++ process/import_export/import_masterlist.php
@@ -2,7 +2,6 @@
 include '../conn.php';
 include '../session.php';
 
-
 $page = '';
 if ($_SESSION['role'] == 'admin_reviewer') {
     $page = 'admin_reviewer';
@@ -15,18 +14,17 @@ if (isset($_POST['upload'])) {
 
     if (!empty($_FILES['file']['name']) && in_array($_FILES['file']['type'], $csvMimes)) {
         if (is_uploaded_file($_FILES['file']['tmp_name'])) {
-            //READ FILE
             $csvFile = fopen($_FILES['file']['tmp_name'], 'r');
-            // SKIP FIRST LINE
-            fgetcsv($csvFile);
-            // PARSE
+            if (!$csvFile) {
+                // Handle file open error
+            }
+            
             $error = 0;
             while (($line = fgetcsv($csvFile)) !== false) {
-
-                if (empty(implode('', $line))) {
+                if (empty(array_filter($line))) {
                     continue;
                 }
-
+                
                 $fullname = $line[0];
                 $m_name = $line[1];
                 $emp_id = $line[2];
@@ -35,106 +33,85 @@ if (isset($_POST['upload'])) {
                 $batch = $line[5];
                 $emp_status = $line[6];
 
+                if (empty($fullname) || empty($emp_id) || empty($agency) || empty($batch)) {
+                    $error++;
+                    continue;
+                }
 
-                // CHECK IF BLANK DATA
-                if ($line[0] == '' || $line[1] == '' || $line[2] == '' || $line[3] == '' || $line[4] == '' || $line[5] == '' || $line[6] == '') {
-                    // IF BLANK DETECTED ERROR += 1
-                    $error = $error + 1;
+                $prevQuery = "SELECT id, emp_id, emp_id_old FROM t_employee_m WHERE emp_id IN (?, ?)";
+                $res = $conn->prepare($prevQuery);
+                if (!$res->execute([$line[2], $line[3]])) {
+                    // Handle query execution error
+                    $error++;
+                    continue;
+                }
+
+                if ($res->rowCount() > 0) {
+                    foreach ($res->fetchAll() as $x) {
+                        $id = $x['id'];
+                        $emp_id_ref = $x['emp_id'];
+                        $emp_id_old_ref = $x['emp_id_old'];
+                    }
+
+                    $updateQuery = "UPDATE t_employee_m SET fullname = ?, emp_id = ?, batch = ?, m_name = ?, agency = ?, emp_status = ?";
+                    $params = [$fullname, $emp_id, $batch, $m_name, $agency, $emp_status];
+
+                    if (!empty($emp_id_old)) {
+                        if (empty($emp_id_old_ref)) {
+                            $updateQuery .= ", emp_id_old = emp_id";
+                        } else if ($emp_id_old != $emp_id_old_ref) {
+                            $updateQuery .= ", emp_id_old = ?";
+                            $params[] = $emp_id_old;
+                        }
+                    }
+
+                    $updateQuery .= " WHERE id = ?";
+                    $params[] = $id;
+
+                    $stmt = $conn->prepare($updateQuery);
+                    if (!$stmt->execute($params)) {
+                        // Handle update query execution error
+                        $error++;
+                        continue;
+                    }
+
+                    $updateFQuery = "UPDATE t_f_process SET emp_id_old = ?, emp_id = ? WHERE emp_id = ?";
+                    $stmt = $conn->prepare($updateFQuery);
+                    $stmt->execute([$emp_id_old, $emp_id, $emp_id_ref]);
+
+                    $updateIQuery = "UPDATE t_i_process SET emp_id_old = ?, emp_id = ? WHERE emp_id = ?";
+                    $stmt = $conn->prepare($updateIQuery);
+                    $stmt->execute([$emp_id_old, $emp_id, $emp_id_ref]);
                 } else {
-                    // CHECK DATA
-                    $prevQuery = "SELECT id, emp_id, emp_id_old FROM t_employee_m WHERE emp_id IN ('$line[2]', '$line[3]')";
-                    $res = $conn->prepare($prevQuery);
-                    $res->execute();
-                    if ($res->rowCount() > 0) {
-                        foreach ($res->fetchAll() as $x) {
-                            $id = $x['id'];
-                            $emp_id_ref = $x['emp_id'];
-                            $emp_id_old_ref = $x['emp_id_old'];
-                        }
-
-                        $update = "UPDATE t_employee_m SET fullname = '$fullname', emp_id = '$emp_id', batch = '$batch', m_name = '$m_name', agency = '$agency', emp_status ='$emp_status', emp_id_old = '$emp_id_old'";
-
-
-                        if (!empty($emp_id_old) && empty($emp_id_old_ref)) {
-                            $update .= ", emp_id_old = '$emp_id'";
-                        } else if (!empty($emp_id_old) && !empty($emp_id_old_ref)) {
-                            $update .= ", emp_id_old = '$emp_id_old'";
-
-                        }
-
-                        $update .= " WHERE id = '$id'";
-
-                        $stmt = $conn->prepare($update);
-                        if ($stmt->execute()) {
-
-                        } else {
-                            $error = $error + 1;
-                        }
-
-                        if (!empty($emp_id_old) && empty($emp_id_old_ref)) {
-                            $query = "UPDATE t_f_process SET emp_id_old = emp_id, emp_id = '$emp_id' WHERE emp_id = '$emp_id_ref'";
-                            $stmt = $conn->prepare($query);
-                            $stmt->execute();
-                            $query = "UPDATE t_i_process SET emp_id_old = emp_id, emp_id = '$emp_id' WHERE emp_id = '$emp_id_ref'";
-                            $stmt = $conn->prepare($query);
-                            $stmt->execute();
-                        } else {
-                            $error = $error + 1;
-                        }
-                    } else {
-
-                        $insert = "INSERT INTO t_employee_m (`fullname` ,`m_name`,`emp_id`, `agency`, `batch`,`emp_status`) VALUES ('$fullname', '$m_name', '$emp_id', '$agency', '$batch','$emp_status')";
-                        $stmt = $conn->prepare($insert);
-                        if ($stmt->execute()) {
-                        } else {
-                            $error = $error + 1;
-                        }
+                    $insertQuery = "INSERT INTO t_employee_m (fullname, m_name, emp_id, agency, batch, emp_status) VALUES (?, ?, ?, ?, ?, ?)";
+                    $stmt = $conn->prepare($insertQuery);
+                    if (!$stmt->execute([$fullname, $m_name, $emp_id, $agency, $batch, $emp_status])) {
+                        // Handle insert query execution error
+                        $error++;
+                        continue;
                     }
                 }
             }
 
-                fclose($csvFile);
-               if($error == 0){
-                    echo '<script>
-                    var x = confirm("SUCCESS! # OF ERRORS '.$error.' ");
-                    if(x == true){
-                        location.replace("../../../'.$page.'/manpowerpage.php");
-                    }else{
-                        location.replace("../../age/'.$page.'/manpowerpage.php");
-                    }
-                </script>'; 
-               }else{
-                    echo '<script>
-                    var x = confirm("WITH ERROR! # OF ERRORS '.$error.' ");
-                    if(x == true){
-                        location.replace("../../page/'.$page.'/manpowerpage.php");
-                    }else{
-                        location.replace("../../page/'.$page.'/manpowerpage.php");
-                    }
-                </script>'; 
-               }
-            }else{
-                echo '<script>
-                        var x = confirm("CSV FILE NOT UPLOADED! # OF ERRORS '.$error.' ");
-                        if(x == true){
-                            location.replace("../../page/'.$page.'/manpowerpage.php");
-                        }else{
-                            location.replace("../../page/'.$page.'/manpowerpage.php");
-                        }
-                    </script>';
-            }
-        }else{
+            fclose($csvFile);
+            $message = ($error == 0) ? "SUCCESS! # OF ERRORS: $error" : "WITH ERROR! # OF ERRORS: $error";
             echo '<script>
-                        var x = confirm("INVALID FILE FORMAT! # OF ERRORS '.$error.' ");
-                        if(x == true){
-                            location.replace("../../page/'.$page.'/manpowerpage.php");
-                        }else{
-                            location.replace("../../page/'.$page.'/manpowerpage.php");
-                        }
-                    </script>';
+                    var x = confirm("' . $message . '");
+                    location.replace("../../page/' . $page . '/manpowerpage.php");
+                  </script>';
+        } else {
+            // Handle file not uploaded error
+            echo '<script>
+                    var x = confirm("CSV FILE NOT UPLOADED! # OF ERRORS ' . $error . ' ");
+                    location.replace("../../page/' . $page . '/manpowerpage.php");
+                  </script>';
         }
-        
+    } else {
+        // Handle invalid file format error
+        echo '<script>
+                var x = confirm("INVALID FILE FORMAT! # OF ERRORS ' . $error . ' ");
+                location.replace("../../page/' . $page . '/manpowerpage.php");
+              </script>';
     }
-
+}
 ?>
- 
