11e9a3f5e51568870ee618a46ce49494021c53c1
jirish18 <irishbautista1801@gmail.com>
2024-03-01 15:54:11 +0800

    export admin_r done

--- page/admin_reviewer/plugins/js/view_script.php
+++ page/admin_reviewer/plugins/js/view_script.php
@@ -243,32 +243,14 @@ const search_data_pagination = () =>{
 //   });     
 // }
 
-function export_data(table_id, separator = ',') {
-    // Select rows from table_id
-    var rows = document.querySelectorAll('table#' + table_id + ' tr');
-    // Construct csv
-    var csv = [];
-    for (var i = 0; i < rows.length; i++) {
-        var row = [], cols = rows[i].querySelectorAll('td, th');
-        for (var j = 0; j < cols.length; j++) {
-            var data = cols[j].innerText.replace(/(\r\n|\n|\r)/gm, '').replace(/(\s\s)/gm, ' ')
-            data = data.replace(/"/g, '""');
-            // Push escaped string
-            row.push('"' + data + '"');
-        }
-        csv.push(row.join(separator));
-    }
-    var csv_string = csv.join('\n');
-    // Download it
-    var filename = 'erecord_data'+ '_' + new Date().toLocaleDateString() + '.csv';
-    var link = document.createElement('a');
-    link.style.display = 'none';
-    link.setAttribute('target', '_blank');
-    link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_string));
-    link.setAttribute('download', filename);
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+const export_data = () => {
+  var emp_id = document.getElementById('emp_id_search').value;
+  var category = document.getElementById('category').value;
+  var pro = document.getElementById('pro').value;
+  var date = document.getElementById('expire_date_search').value;
+  var date_authorized = document.getElementById('date_authorized_search').value;
+  var fullname = document.getElementById('fullname_search').value;
+  window.open('../../process/import_export/export_view_data.php?emp_id='+emp_id+"&category="+category+"&pro="+pro+"&date="+date+"&date_authorized="+date_authorized+"&fullname="+fullname,'_blank');
 }
   
   
